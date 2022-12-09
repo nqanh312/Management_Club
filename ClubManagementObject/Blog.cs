f0ff1e1cc43a27daaa00f8e5ee1d364644da6238
nqanh312 <nqanh312@gmail.com>
2022-12-09 17:42:35 +0700

    blog management

---
+++ ClubManagementObject/Blog.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace ClubManagementObject
+{
+    public class Blog
+    {   
+        
+
+        [Key]
+        public int BlogId { get; set; }
+
+        [Required(ErrorMessage = "Ten tac gia bai viet khong duoc rong")]
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? PublicDays { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(4000)]
+        [AllowHtml]
+        [Required(ErrorMessage = "Noi dung khong duoc rong")]
+        public string Content { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(100)]
+        [Required(ErrorMessage = "Tieu de khong duoc rong")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(100)]
+        public string Image { get; set; }
+        
+        [NotMapped]
+        [DisplayName("Upload File")]
+        [Required(ErrorMessage = "Anh thumnail khong duoc trong")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
---
+++ ClubManagementObject/Point.cs
@@ -0,0 +1,25 @@
+﻿using ClubManagementObject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Point
+    {
+        [Key]
+        public int PointId { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public int SumPoint { get; set; }
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)] 
+        public string Semeter { get; set; }
+
+    }
+}
